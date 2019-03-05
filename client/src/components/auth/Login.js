4267d70385845345cab4c560c9fe2f05fe769b66
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-05 11:54:17 +0200

    creating the login and register state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    errors: {}
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(user);
+  };
+
   render() {
     return (
       <div className="login">
         <div className="container">
           <h1 className="form__title">Log In</h1>
           <p className="form__subtitle">Sign in to your account</p>
-          <form action="#">
+          <form onSubmit={this.onSubmit}>
             <div className="login__forms">
               <input
                 type="email"
                 className="form__input"
                 placeholder="Email Address"
                 name="email"
+                value={this.state.email}
+                onChange={this.onChange}
               />
             </div>
             <div className="login__forms">
@@ -22,6 +44,8 @@ class Login extends Component {
                 className="form__input"
                 placeholder="Password"
                 name="password"
+                value={this.state.password}
+                onChange={this.onChange}
               />
             </div>
             <input type="submit" className="btn form--btn" />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+    console.log(newUser);
+  };
+
   render() {
     return (
       <div className="register">
         <div className="container">
           <h1 className="form__title">Sign Up</h1>
           <p className="form__subtitle">Create your workout account</p>
-          <form action="#">
+          <form onSubmit={this.onSubmit}>
             <div className="register__forms">
               <input
                 type="text"
                 className="form__input"
                 placeholder="Name"
                 name="name"
+                value={this.state.name}
+                onChange={this.onChange}
               />
             </div>
             <div className="register__forms">
@@ -22,6 +48,8 @@ class Register extends Component {
                 className="form__input"
                 placeholder="Email Address"
                 name="email"
+                value={this.state.email}
+                onChange={this.onChange}
               />
             </div>
             <div className="register__forms">
@@ -30,6 +58,8 @@ class Register extends Component {
                 className="form__input"
                 placeholder="Password"
                 name="password"
+                value={this.state.password}
+                onChange={this.onChange}
               />
             </div>
             <div className="register__forms">
@@ -38,6 +68,8 @@ class Register extends Component {
                 className="form__input"
                 placeholder="Confirm Password"
                 name="password2"
+                value={this.state.password2}
+                onChange={this.onChange}
               />
             </div>
             <input type="submit" className="btn form--btn" />
