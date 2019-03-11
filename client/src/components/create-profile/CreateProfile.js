c3e8e050cc87cf01ea2e90bd312ad747eae44e78
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-11 11:11:31 +0200

    create profile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import { createProfile } from '../../actions/profileActions';
 import { withRouter } from 'react-router-dom';
 
 class CreateProfile extends Component {
   state = {
-    displaySocialInputs: false,
     handle: '',
     location: '',
     style: '',
@@ -14,7 +15,33 @@ class CreateProfile extends Component {
     gym: '',
     errors: {}
   };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+  onSubmit = e => {
+    e.preventDefault();
+
+    const profileData = {
+      handle: this.state.handle,
+      gym: this.state.gym,
+      location: this.state.location,
+      bio: this.state.bio,
+      style: this.state.style
+    };
+
+    this.props.createProfile(profileData, this.props.history);
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="create-profile">
         <div className="container">
@@ -23,6 +50,53 @@ class CreateProfile extends Component {
               <h1 className="display-4 text-center">Create Your Profile</h1>
               <p className="lead text-center">Lets get some info about you</p>
               <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL."
+                />
+                <TextFieldGroup
+                  placeholder="Location"
+                  name="location"
+                  value={this.state.location}
+                  onChange={this.onChange}
+                  error={errors.location}
+                  info="The City you are from"
+                />
+                <TextFieldGroup
+                  placeholder="Gym"
+                  name="gym"
+                  value={this.state.gym}
+                  onChange={this.onChange}
+                  error={errors.gym}
+                  info="The gym you are going"
+                />
+                <TextAreaFieldGroup
+                  placeholder="Short Bio"
+                  name="bio"
+                  value={this.state.bio}
+                  onChange={this.onChange}
+                  error={errors.bio}
+                  info="Tell us a little about yourself"
+                />
+                <TextAreaFieldGroup
+                  placeholder="Workout style"
+                  name="style"
+                  value={this.state.style}
+                  onChange={this.onChange}
+                  error={errors.style}
+                  info="Tell us a little about your workout"
+                />
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block my-4"
+                />
+              </form>
             </div>
           </div>
         </div>
@@ -41,6 +115,6 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 export default connect(
-  mapStateToProps
-  // { createProfile }
+  mapStateToProps,
+  { createProfile }
 )(withRouter(CreateProfile));
