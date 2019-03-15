8558a1e5a86348373780fea429af25f21aa37866
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-15 22:12:09 +0200

    did not fix the problem

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    'mongodb://Alex-Selariu-32:323232bjj@cluster0-shard-00-00-rgmh2.mongodb.net:27017,cluster0-shard-00-01-rgmh2.mongodb.net:27017,cluster0-shard-00-02-rgmh2.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
-  secretOrKey: 'SECRET'
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
 };
