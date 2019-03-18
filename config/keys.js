92af35694b93d9ce1a7063968beb7d77ca40ae79
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-18 22:04:54 +0200

    modified keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,11 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+
+module.exports = {
+  mongoURI:
+    'mongodb://Alex-Selariu-32:323232bjj@cluster0-shard-00-00-rgmh2.mongodb.net:27017,cluster0-shard-00-01-rgmh2.mongodb.net:27017,cluster0-shard-00-02-rgmh2.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: 'SECRET'
+};
